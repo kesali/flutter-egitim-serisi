b05b1b8be24d1093a4b5e3cbaccf320c6efe9bb8
Dursunali KESKİN <kesali@gmail.com>
2018-12-23 14:56:23 +0300

    birkaç ufak görsel düzeltme ve divider düzeltmesi

--- phone_book 1/lib/main.dart
+++ phone_book 1/lib/main.dart
@@ -33,38 +33,47 @@ class MyApp extends StatelessWidget {
         title: Text("Phone Book"),
         actions: <Widget>[IconButton(icon: Icon(Icons.add), onPressed: () {})],
       ),
-      body: Container(
-        child: ListView.builder(
-            itemCount: contacts.length,
-            itemBuilder: (BuildContext context, int index) {
-              Contact contact = contacts[index];
-              return Container(
-                padding: EdgeInsets.all(5),
-                child: Row(
-                  children: <Widget>[
-                    CircleAvatar(
-                      backgroundImage: NetworkImage(
-                        "https://placekitten.com/200/200",
+      body: ListView.builder(
+          itemCount: contacts.length,
+          itemBuilder: (BuildContext context, int index) {
+            Contact contact = contacts[index];
+            return Container(
+              padding: EdgeInsets.all(5),
+              child: Column(
+                children: <Widget>[
+                  Row(
+                    children: <Widget>[
+                      CircleAvatar(
+                        backgroundImage: NetworkImage(
+                          "https://placekitten.com/200/200",
+                        ),
+                        child: Text(
+                          contact.name[0],
+                          style: TextStyle(fontSize: 24, fontWeight: FontWeight.bold),
+                        ),
                       ),
-                      child: Text(
-                        contact.name[0],
-                        style: TextStyle(fontSize: 24, fontWeight: FontWeight.bold),
+                      Padding(
+                        padding: const EdgeInsets.only(left: 8),
+                        child: Column(
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          children: <Widget>[
+                            Text(contact.name),
+                            Text(contact.phoneNumber),
+                          ],
+                        ),
                       ),
+                    ],
+                  ),
+                  Padding(
+                    padding: const EdgeInsets.only(top: 8.0),
+                    child: Divider(
+                      height: 2,
                     ),
-                    Padding(
-                      padding: const EdgeInsets.only(left: 8),
-                      child: Column(
-                        crossAxisAlignment: CrossAxisAlignment.start,
-                        children: <Widget>[Text(contact.name), Text(contact.phoneNumber)],
-                      ),
-                    ),
-                    Divider(),
-                  ],
-                ),
-              );
-              ;
-            }),
-      ),
+                  ),
+                ],
+              ),
+            );
+          }),
     ));
   }
 }
