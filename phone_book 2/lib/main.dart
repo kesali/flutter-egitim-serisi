2501b6b3df0fc0fae62f9302a47293c5e595daa5
Dursunali KESKİN <kesali@gmail.com>
2018-12-24 00:09:45 +0300

    4. video dosyaları

---
+++ phone_book 2/lib/main.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+import 'package:phone_book/contact_page.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  Widget build(BuildContext context) {
+    return MaterialApp(
+        home: ContactPage()
+    );
+  }
+}
---
+++ phone_book 2/lib/model/contact.dart
@@ -0,0 +1,22 @@
+class Contact {
+  String name;
+  String phoneNumber;
+
+  static List<Contact> contacts = [
+    Contact(name: "Ali Kemal", phoneNumber: "0555 555 55 55"),
+    Contact(name: "Dali", phoneNumber: "0555 555 55 55"),
+    Contact(name: "Emma", phoneNumber: "0555 555 55 55"),
+    Contact(name: "Ergun", phoneNumber: "0555 555 55 55"),
+    Contact(name: "Esra", phoneNumber: "0555 555 55 55"),
+    Contact(name: "Fatih", phoneNumber: "0555 555 55 55"),
+    Contact(name: "Kadir", phoneNumber: "0555 555 55 55"),
+    Contact(name: "Kübra", phoneNumber: "0555 555 55 55"),
+    Contact(name: "Mehmet", phoneNumber: "0555 555 55 55"),
+    Contact(name: "Münire", phoneNumber: "0555 555 55 55"),
+    Contact(name: "Sema", phoneNumber: "0555 555 55 55"),
+    Contact(name: "Yasir", phoneNumber: "0555 555 55 55"),
+    Contact(name: "Zeynep", phoneNumber: "0555 555 55 55"),
+  ];
+
+  Contact({this.name, this.phoneNumber});
+}
