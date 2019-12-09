47ed7dea92c7a3b09243098fbd978989cfa5ae18
Dursunali Keskin <kesali@gmail.com>
2019-12-09 20:17:26 +0300

    video dosyaları

--- whatsapp_04/lib/screens/conversation_page.dart
+++ whatsapp_04/lib/screens/conversation_page.dart
@@ -1,57 +1,64 @@
 import 'package:flutter/material.dart';
 
 class ConversationPage extends StatelessWidget {
-  const ConversationPage({Key key, String conversationId}) : super(key: key);
+  const ConversationPage({Key key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        titleSpacing: -10,
+        titleSpacing: -5,
         title: Row(
-          mainAxisAlignment: MainAxisAlignment.start,
           children: <Widget>[
             CircleAvatar(
               backgroundImage: NetworkImage("https://placekitten.com/200/200"),
             ),
             Padding(
               padding: const EdgeInsets.only(left: 8.0),
-              child: Text("Emma Amanda"),
-            ),
+              child: Text("Dursunali Keskin"),
+            )
           ],
         ),
         actions: <Widget>[
-          InkWell(
-            onTap: () {},
-            child: Icon(Icons.video_call),
+          Padding(
+            padding: const EdgeInsets.all(4.0),
+            child: InkWell(
+              child: Icon(Icons.phone),
+              onTap: () {},
+            ),
           ),
-          InkWell(
-            onTap: () {},
-            child: Icon(Icons.call),
+          Padding(
+            padding: const EdgeInsets.all(4.0),
+            child: InkWell(
+              child: Icon(Icons.camera_alt),
+              onTap: () {},
+            ),
           ),
-          InkWell(
-            onTap: () {},
-            child: Icon(Icons.more_vert),
-          ),
+          Padding(
+            padding: const EdgeInsets.all(4.0),
+            child: InkWell(
+              child: Icon(Icons.more_vert),
+              onTap: () {},
+            ),
+          )
         ],
       ),
       body: Container(
         decoration: BoxDecoration(
-          image: DecorationImage(
-            fit: BoxFit.cover,
-            image: NetworkImage("https://placekitten.com/600/800"),
-          ),
-        ),
+            image: DecorationImage(
+                fit: BoxFit.cover,
+                image: NetworkImage("https://placekitten.com/600/800"))),
         child: Column(
           children: <Widget>[
             Expanded(
               child: ListView.builder(
+                itemCount: 10,
                 itemBuilder: (BuildContext context, int index) {
                   return ListTile(
                     title: Align(
                         alignment: index % 2 == 0
-                            ? Alignment.centerRight
-                            : Alignment.bottomLeft,
+                            ? Alignment.centerLeft
+                            : Alignment.centerRight,
                         child: Container(
                             padding: EdgeInsets.all(8),
                             decoration: BoxDecoration(
@@ -60,12 +67,11 @@ class ConversationPage extends StatelessWidget {
                                     left: Radius.circular(10),
                                     right: Radius.circular(10))),
                             child: Text(
-                              "asda",
+                              "Deneme Mesajı",
                               style: TextStyle(color: Colors.white),
                             ))),
                   );
                 },
-                itemCount: 10,
               ),
             ),
             Row(
@@ -73,58 +79,61 @@ class ConversationPage extends StatelessWidget {
                 Expanded(
                   child: Container(
                     margin: EdgeInsets.all(5),
-                    height: 40,
                     decoration: BoxDecoration(
-                        color: Colors.white,
-                        borderRadius: BorderRadius.horizontal(
-                            left: Radius.circular(25),
-                            right: Radius.circular(25))),
+                      color: Colors.white,
+                      borderRadius: BorderRadius.horizontal(
+                        left: Radius.circular(25),
+                        right: Radius.circular(25),
+                      ),
+                    ),
                     child: Row(
                       children: <Widget>[
-                        InkWell(
-                          onTap: () {},
-                          child: Padding(
-                            padding: const EdgeInsets.symmetric(horizontal: 8),
-                            child: Icon(Icons.tag_faces, color: Colors.grey),
+                        Padding(
+                          padding: const EdgeInsets.all(8.0),
+                          child: InkWell(
+                            child: Icon(
+                              Icons.tag_faces,
+                              color: Colors.grey,
+                            ),
                           ),
                         ),
                         Expanded(
-                            child: TextField(
-                          decoration: InputDecoration(
-                              hintText: "Type a message",
-                              border: InputBorder.none),
-                        )),
-                        InkWell(
-                          onTap: () {},
-                          child: Padding(
-                            padding: const EdgeInsets.symmetric(horizontal: 8),
-                            child: Icon(Icons.attach_file, color: Colors.grey),
-                          ),
+                          child: TextField(
+                              decoration: InputDecoration(
+                                  hintText: "Type a message",
+                                  border: InputBorder.none)),
                         ),
                         InkWell(
-                          onTap: () {},
-                          child: Padding(
-                            padding: const EdgeInsets.symmetric(horizontal: 8),
-                            child: Icon(Icons.camera_alt, color: Colors.grey),
+                          child: Icon(
+                            Icons.attach_file,
+                            color: Colors.grey,
                           ),
                         ),
+                        Padding(
+                          padding: const EdgeInsets.all(8.0),
+                          child: InkWell(
+                            child: Icon(
+                              Icons.camera_alt,
+                              color: Colors.grey,
+                            ),
+                          ),
+                        )
                       ],
                     ),
                   ),
                 ),
                 Container(
                   margin: EdgeInsets.only(right: 5),
-                  padding: EdgeInsets.all(8),
                   decoration: BoxDecoration(
                     shape: BoxShape.circle,
                     color: Theme.of(context).primaryColor,
                   ),
-                  child: InkWell(
-                    child: Icon(
+                  child: IconButton(
+                    icon: Icon(
                       Icons.mic,
                       color: Colors.white,
                     ),
-                    onTap: () {},
+                    onPressed: () {},
                   ),
                 )
               ],
