0809a4c938ed51b6b58bee99080d7069a891c731
Dursunali Keskin <kesali@gmail.com>
2020-05-25 17:23:30 +0300

    whatsapp 10 video dosyaları

---
+++ whatsapp_10/lib/core/locator.dart
@@ -0,0 +1,21 @@
+import 'package:get_it/get_it.dart';
+import 'package:whatsapp_clone/viewmodels/chats_model.dart';
+import 'package:whatsapp_clone/viewmodels/contacts_model.dart';
+import 'package:whatsapp_clone/viewmodels/main_model.dart';
+import 'package:whatsapp_clone/viewmodels/sign_in_model.dart';
+import 'package:whatsapp_clone/core/services/chat_service.dart';
+import 'package:whatsapp_clone/core/services/auth_service.dart';
+import 'package:whatsapp_clone/core/services/navigator_service.dart';
+
+GetIt getIt = GetIt.instance;
+
+setupLocators() {
+  getIt.registerLazySingleton(() => NavigatorService());
+  getIt.registerLazySingleton(() => ChatService());
+  getIt.registerLazySingleton(() => AuthService());
+
+  getIt.registerFactory(() => MainModel());
+  getIt.registerFactory(() => ChatsModel());
+  getIt.registerFactory(() => SignInModel());
+  getIt.registerFactory(() => ContactsModel());
+}
---
+++ whatsapp_10/lib/main.dart
@@ -0,0 +1,36 @@
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+import 'package:whatsapp_clone/core/locator.dart';
+import 'package:whatsapp_clone/screens/sign_in_page.dart';
+import 'package:whatsapp_clone/core/services/navigator_service.dart';
+import 'package:whatsapp_clone/screens/whatsapp_main.dart';
+import 'package:whatsapp_clone/viewmodels/sign_in_model.dart';
+
+void main() {
+  setupLocators();
+
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return FutureProvider(
+      create: (BuildContext context) => getIt<SignInModel>().currentUser,
+      child: MaterialApp(
+        title: 'WhatsApp Clone',
+        navigatorKey: getIt<NavigatorService>().navigatorKey,
+        theme: ThemeData(
+          primaryColor: Color(0xff075E54),
+          accentColor: Color(0xff25D366),
+        ),
+        home: Consumer<FirebaseUser>(
+          builder: (BuildContext context, FirebaseUser user, Widget child) =>
+              user == null ? SignInPage() : WhatsAppMain(),
+        ),
+      ),
+    );
+  }
+}
